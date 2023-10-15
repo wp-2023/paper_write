de69f9a5504debcecb33735afede0e6ca88a2023
wp-2023 <wpycharm@icloud.com>
2023-10-15 14:47:47 +0800

    更新ci测试的代码
    action不通过会报错

--- validate_references.py
+++ validate_references.py
@@ -5,15 +5,20 @@ import re
 3. Brown, Robert. "A Book on Samples." Publisher XYZ, 2019.
 """
 
+class InvalidReferenceFormatError(Exception):
+    pass
+
 def validate_references(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         # 此处可以使用正则表达式或其他方法来验证参考文献的格式
         # 这只是一个示例，你需要根据你的实际需求进行修改
-        if re.search(r'\d+\.\s[^\n]+', content):
-            print("参考文献格式正确")
-        else:
-            print("参考文献格式不正确")
+        if not re.search(r'\d+\.\s[^\n]+', content):
+            raise InvalidReferenceFormatError("参考文献格式不正确")
 
 if __name__ == "__main__":
-    validate_references('参考文献.txt')
+    try:
+        validate_references('参考文献.txt')
+    except InvalidReferenceFormatError as e:
+        print(e)
+        exit(1)
